05f47f230bdb4892b62e9b2c8c46de149c38c9fb
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-07-07 13:21:50 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Bill", "Ian", "Tom"];
+const events = ["surprise"]
+
+function writeCards(names) {
+  let final = []
+    for (let i = 0; i < names.length; i++) {
+    final.push (`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+  }
+  return final
+}
+
+let countdown = 10; 
+function countDown(countdown) {
+    while (countdown > -1) {
+    console.log(countdown--)
+    }
+}
